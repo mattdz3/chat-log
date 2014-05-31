eaf7f2fec693c2cc50db505abc4f522c76a5b22c
Matt Dougherty <mattz325@gmail.com>
2014-05-31 11:29:56 -0400

    now loads to the page

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,40 +1,37 @@
 'use strict';
 
-var pizzaServer;
 
 var user = "TheHamBurgler";
 
-var chatLog = _.template($('.input-box').text());
+var chatLog = _.template($('.chat').text());
 
+function loadChat() {
+
+$.getJSON('http://tiny-pizza-server.herokuapp.com/collections/chat-messages').done(function(data) {
+
+  renderChat(data);
+
+  });
+}
 
 function renderChat(data) {
   $('.container').empty();
-
-  for (var i = 0; i < data.length - 1; i += 1) {
+    var i
+  for (i = 0; i < data.length - 1; i += 1) {
 
       if (data[i].message) {
 
       var render = chatLog(data[i]);
 
-      $('.container').append(render);
+      $('.container').prepend(render);
     }
   }
 }
 
-function loadChat() {
-
-$.getJSON('http://tiny-pizza-server.herokuapp.com/collections/chat-messages').done(function(data) {
-	
-	pizzaServer = data;
-
-	loadChat(data);
-	});
-}
-
 function ChatObject (user, message, time) {
-	this.user = user || '';
-	this.message = message || '';
-	this.time = time || '';	
+	this.user = user;
+	this.message = message;
+	this.time = time;	
 }
 
 function refresh(info) {
@@ -48,9 +45,9 @@ $('.submit').click(function() {
 
   var time = Date.now();
 
-  var outgoing = new ChatObject(user, message, time);
+  var sentMessage = new ChatObject(user, message, time);
 
-  refresh(outgoing);
+  refresh(sentMessage);
 })
 
 
